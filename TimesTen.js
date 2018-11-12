85a0a66d547ea0c0bf91a22331def0f9e46ebda3
Fiyaz Hasan <fiyazhasan@live.com>
2018-11-12 15:29:41 +0600

    minor changes to cache and memoization sample

--- TimesTen.js
+++ TimesTen.js
@@ -1,76 +1,74 @@
-// const times10 = n => {
-//   return n * 10;
-// };
-// console.log(times10(5));
+const times10 = n => {
+  return n * 10;
+};
+console.log(times10(5));
 
-// const cache = {};
+const cache = {};
 
-// const memoTimes10 = n => {
-//   if (!cache[n]) {
-//     cache[n] = n * 10;
-//     return cache[n];
-//   } else return cache[n];
-// };
+const memoTimes10 = n => {
+  if (!cache[n]) {
+    cache[n] = n * 10;
+    return cache[n];
+  } else return cache[n];
+};
 
-// console.log(memoTimes10(5));
-// console.log(memoTimes10(4));
-// console.log(memoTimes10(5));
+console.log(memoTimes10(5));
+console.log(memoTimes10(4));
+console.log(memoTimes10(5));
 
-// const cache = {};
+const memoTimes10V2 = n => {
+  if (n in cache) {
+    return cache[n];
+  } else {
+    let result = n * 10;
+    cache[n] = result;
+    return result;
+  }
+};
 
-// const memoTimes10V2 = n => {
-//   if (n in cache) {
-//     return cache[n];
-//   } else {
-//     let result = n * 10;
-//     cache[n] = result;
-//     return result;
-//   }
-// };
+console.log(memoTimes10V2(5));
+console.log(memoTimes10V2(4));
+console.log(memoTimes10V2(5));
 
-// console.log(memoTimes10V2(5));
-// console.log(memoTimes10V2(4));
-// console.log(memoTimes10V2(5));
+const memoTimes10V3 = () => {
+  let cache = {};
+  return n => {
+    if (n in cache) {
+      console.log('Returning from cache');
+      return cache[n];
+    } else {
+      console.log('Storing in cache');
+      let result = n * 10;
+      cache[n] = result;
+      return result;
+    }
+  };
+};
 
-// const memoTimes10V3 = () => {
-//   let cache = {};
-//   return n => {
-//     if (n in cache) {
-//       console.log('Returning from cache');
-//       return cache[n];
-//     } else {
-//       console.log('Storing in cache');
-//       let result = n * 10;
-//       cache[n] = result;
-//       return result;
-//     }
-//   };
-// };
+var memoTimes10Closure = memoTimes10V3();
+console.log(memoTimes10Closure(3));
+console.log(memoTimes10Closure(5));
+console.log(memoTimes10Closure(3));
 
-// var memoTimes10Closure = memoTimes10V3();
-// console.log(memoTimes10Closure(3));
-// console.log(memoTimes10Closure(5));
-// console.log(memoTimes10Closure(3));
+const memoTimesMultiplier = m => {
+  let cache = {};
+  return n => {
+    if (n in cache) {
+      console.log('Returning from cache');
+      return cache[n];
+    } else {
+      console.log('Storing in cache');
+      let result = n * m;
+      cache[n] = result;
+      return result;
+    }
+  };
+};
 
-// const memoTimesMultiplier = m => {
-//   let cache = {};
-//   return n => {
-//     if (n in cache) {
-//       console.log('Returning from cache');
-//       return cache[n];
-//     } else {
-//       console.log('Storing in cache');
-//       let result = n * m;
-//       cache[n] = result;
-//       return result;
-//     }
-//   };
-// };
-
-// var memoTimes4Closure = memoTimesMultiplier(4);
-// console.log(memoTimes4Closure(3));
-// console.log(memoTimes4Closure(5));
-// console.log(memoTimes4Closure(3));
+var memoTimes4Closure = memoTimesMultiplier(4);
+console.log(memoTimes4Closure(3));
+console.log(memoTimes4Closure(5));
+console.log(memoTimes4Closure(3));
 
 const memoizeCallback = cb => {
   let cache = {};
