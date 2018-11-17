c45b9a65a4c3bf4bfbe7c391041c542133e6221c
fiyazbinhasan <fiyazhasan@live.com>
2018-11-17 23:05:22 +0600

    better implementation for rod cutting algorithm dynamic way

--- CutRodDynamic.js
+++ CutRodDynamic.js
@@ -1,16 +1,18 @@
 const lengths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30];
 const cache = {};
+lengths.unshift(0);
+prices.unshift(0);
 
 const cutRod = (prices, n) => {
   if (cache[n]) return cache[n];
   let maxProfit = -1;
   if (n == 0) maxProfit = 0;
   for (let i = 1; i <= n; i++) {
-    maxProfit = Math.max(maxProfit, prices[i - 1] + cutRod(prices, n - i));
+    maxProfit = Math.max(maxProfit, prices[i] + cutRod(prices, n - i));
   }
   cache[n] = maxProfit;
   return cache[n];
 };
 
-console.log(cutRod(prices, 5));
+console.log(cutRod(prices, 4));
